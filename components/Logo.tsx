5213a13cb8ce20a796caf3d3125d15bf1ff293f9
cosre7 <cosre7@gmail.com>
2025-11-05 11:07:52 +0900

    Fix : 로고 클릭 시 home 화면으로 이동 경로 설정

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import React from "react";
-import { motion, useReducedMotion } from "framer-motion";
 
-export function LogoConnected({ size = 28, amp = 3, period = 2.2 }) {
-    const reduced = useReducedMotion();
-    const boxes = [2, 18, 34];
-    const wave = reduced ? [0] : [0, -amp, 0, amp, 0];
-    return <img src="/images/logo.png" className="w-[7rem]"></img>;
+export function LogoConnected() {
+    return (
+        <a href="/">
+            <img src="/images/logo.png" className="w-[7rem]"></img>
+        </a>
+    );
 }
